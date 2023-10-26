600a22bdabad36c5070972b43f9f565fd1572e5c
suryanshsingh49 <133967536+suryanshsingh49@users.noreply.github.com>
2023-10-26 16:50:03 +0000

    variables and datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); console.log("Suryansh");
+
+let name = "Suryansh"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
