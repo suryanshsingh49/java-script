9ceaf54d0e50dd4f0d0ee537e0ce060cb854d6db
suryanshsingh49 <133967536+suryanshsingh49@users.noreply.github.com>
2023-10-31 17:45:07 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// Array
+
+const myArr =  [0,1,2,3,4,5];
+const myHeroes = ["IronMan", "SpiderMan", "Shaktiman"];
+const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[4]);
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9))
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+const myn1 = myArr.slice(1,3)
+console.log("A", myArr)
+console.log(myn1)
+
+console.log("B", myArr)
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr)
+console.log(myn2)
+
+
