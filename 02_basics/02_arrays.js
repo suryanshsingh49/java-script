60f4add029d577c635aba9a40e36aa66b362a1bb
suryanshsingh49 <133967536+suryanshsingh49@users.noreply.github.com>
2023-10-31 18:32:42 +0000

    arrays more methods

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+
+// console.log(all_new_heroes);
+
+const another_array = [1,2,3,[4,5,6],7, [6,7[4,5]]]
+const flatArray = another_array.flat(Infinity)
+console.log(flatArray);
+
+console.log(Array.isArray("Suryansh"))
+console.log(Array.from("Suryansh"))
+console.log(Array.from({name: "Suryansh"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
